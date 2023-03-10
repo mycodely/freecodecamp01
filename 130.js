be41680aa8cab54f4fc7c45919951b2e7d109529
mycodely <122876501+mycodely@users.noreply.github.com>
2023-03-10 00:07:13 -0300

    Repeat a String Repeat a String (refactor version)
    Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.

--- 130.js
+++ 130.js
@@ -1,9 +1,21 @@
+/*
 function repeatStringNumTimes(str, num) {
   let result = "";
   if(num > 0){
     for(let i = 0; i < num; i++){
       result += str;
     }
+  }
+  return result;
+}
+*/
+
+//v2
+
+function repeatStringNumTimes(str, num) {
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += str;
   }
   return result;
 }
